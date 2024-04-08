13a6aaa6a1ee5848b238f164c6ddb48fdaf855ea
Bruno Héritier <brunoheritier@MacBook-Pro-de-Bruno.local>
2024-04-08 11:24:38 +0200

    exo 1

---
+++ js/index.js
@@ -0,0 +1,33 @@
+// const car = {
+//   wheels: 4,
+//   steeringWheel: 1,
+//   maxSpeed: 240,
+//   color: "white",
+//   engineIsOn: false,
+//   start() {
+//     console.log(this);
+//     this.engineIsOn = true;
+//     console.log("vroum vroum");
+//   },
+//   stop: () => {
+//     //this.engineIsOn = false;
+//   }
+// };
+
+// const redCar = Object.create(car);
+// redCar.color = "red";
+// console.log(redCar.maxSpeed, redCar.color);
+
+// const greenCar = Object.create(car);
+// greenCar.color = "green";
+// console.log(greenCar.maxSpeed, greenCar.color);
+
+// car.maxSpeed = 210;
+
+// const blackCar = Object.create(car);
+// blackCar.color = "black";
+// console.log(blackCar.maxSpeed, blackCar.color);
+
+// Transposez le code ci-dessus en "function contructor"
+// lien utile : https://www.w3schools.com/js/js_object_constructors.asp
+

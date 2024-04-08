fbcee5f893d6c95e7c3ee20fc32ecb61a76eeb21
Bruno Héritier <brunoheritier@MacBook-Pro-de-Bruno.local>
2024-04-08 11:44:20 +0200

    corrige exo 1

---
+++ js/activite1-corrige.js
@@ -0,0 +1,28 @@
+function Car(color = "white") {
+  this.wheels = 4;
+  this.steeringWheel = 1;
+  this.maxSpeed = 240;
+  this.color = color;
+  this.engineIsOn = false;
+};
+
+Car.prototype.start = function () {
+  console.log(this);
+  this.engineIsOn = true;
+  console.log("vroum vroum");
+};
+
+Car.prototype.stop = function () {
+  this.engineIsOn = false;
+};
+
+const redCar = new Car("red");
+console.log(redCar.maxSpeed, redCar.color);
+
+const greenCar = new Car("green");
+console.log(greenCar.maxSpeed, greenCar.color);
+
+Car.prototype.maxSpeed = 100;
+
+const blackCar = new Car("black");
+console.log(blackCar.maxSpeed, blackCar.color);
